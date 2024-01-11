f83e9775bd4379385227dee326b2f8015944bab7
isunder <51404113+isunder@users.noreply.github.com>
2024-01-12 00:55:29 +0530

    some changes

--- Socket-Server/src/routes/socket.route.js
+++ Socket-Server/src/routes/socket.route.js
@@ -3,23 +3,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    global.io.emit('home', { success: true, message: "Hello", type: 'info' })
+    global.io.emit('home', { success: true, message: "hello from socket IO", type: 'info' })
     res.send({ success: true, message: "socket" })
 })
 
 router.get('/info', (req, res) => {
-    global.io.emit('info', { success: true, message: "info", type: 'info' })
+    global.io.emit('info', { success: true, message: "info Message from Socket", type: 'info' })
 
     res.send({ success: true, message: "socket" })
 })
 
 router.get('/error', (req, res) => {
-    global.io.emit('error', { success: true, message: "error", type: 'error' })
+    global.io.emit('error', { success: true, message: "Error Message from Socket", type: 'error' })
 
     res.send({ success: true, message: "socket" })
 })
 router.get('/success', (req, res) => {
-    global.io.emit('success', { success: true, message: "success", type: 'success' })
+    global.io.emit('success', { success: true, message: "success fully installed socket enjoy", type: 'success' })
 
     res.send({ success: true, message: "socket" })
 })
---
+++ readme.txt
@@ -0,0 +1,14 @@
+Step 1 : Install NodeModules of both client and server
+Step 2 : run client by running npm start
+Step 3 : run server by running npm start
+Step 4 : Navigate to Frontend by going localhost:3000
+Step 5 : After running the backend you will se console when you reloaded the app saying user connected
+Step 6 : now Open postman and hit different routes and see the action being called without any refresh on the frontend 
+
+
+The Routes are as follows: 
+
+http://localhost:8888/socket
+http://localhost:8888/socket/info
+http://localhost:8888/socket/error
+http://localhost:8888/socket/success
